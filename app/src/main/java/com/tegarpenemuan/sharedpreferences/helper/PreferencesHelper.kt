c164a1410e68d65dee5aa3629eb753170476e7d2
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-27 18:58:18 +0700

    PreferencesHelper

---
+++ app/src/main/java/com/tegarpenemuan/sharedpreferences/helper/PreferencesHelper.kt
@@ -0,0 +1,33 @@
+package com.tegarpenemuan.sharedpreferences.helper
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferencesHelper (context: Context) {
+    private val PREFS_NAME = "sharedprefkotlin12345"
+    private var sharedpref: SharedPreferences
+    val editor: SharedPreferences.Editor
+
+    init {
+        sharedpref = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        editor = sharedpref.edit()
+    }
+
+    fun put(key:String, value:String) {
+        editor.putString(key,value)
+            .apply()
+    }
+
+    fun getString(key: String):String? {
+        return sharedpref.getString(key,null)
+    }
+
+    fun put(key:String, value:Boolean) {
+        editor.putBoolean(key,value)
+            .apply()
+    }
+
+    fun getBoolean(key: String):Boolean? {
+        return sharedpref.getBoolean(key,false)
+    }
+}
